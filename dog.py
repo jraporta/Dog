94799ec5ca533970a625782af59f1d1de064b802
jraporta <jraporta@gmail.com>
2019-10-19 18:58:17 +0000

    Enunciado del ejercicio sin modificaciones.

---
+++ dog.py
@@ -0,0 +1,15 @@
+class Dog:
+    species = 'caniche'
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+bambi = Dog("Bambi", 5)
+mikey = Dog("Rufus", 6)
+
+print("{} is {} and {} is {}.". format(bambi.name, bambi.age, mikey.name, mikey.age))
+
+if bambi.species == "caniche":
+    print("{0} is a {1}!".format(bambi.name, bambi.species))
---
+++ readme.md
@@ -0,0 +1,1 @@
+Esto es un ejercicio para aprender los fundamentos de python y el uso de git.
